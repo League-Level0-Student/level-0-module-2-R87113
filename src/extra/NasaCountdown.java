b47c918d71b54d366c5c8ac653e91dd6736f013d
Daniel Commins <dencee@users.noreply.github.com>
2019-01-16 17:42:15 -0800

    ok

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -15,25 +17,31 @@ package extra;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+		String Ok = JOptionPane.showInputDialog("Where would you like the count down to start from?");
 		// 3. Change the countdown to use the new starting point
-		
+		int a = Integer.parseInt(Ok);
 		// 1. Print a countdown from 10 to 0 on the console
-	
-			// 4. Use the speak method to hear the countdown.
-		
-			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-		
-		// 5. when the counting is done, speak "blastoff!"
+		for (int i = a; i >= 0; i--) {
+			System.out.println(i);
+			speak("" + i);
+			Thread.sleep(1000);
+		}
+
+		speak("Blast off");
 	}
 
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
+	// 4. Use the speak method to hear the countdown.
+
+	// 6. Use the following code to make the program wait one second for each
+	// number: Thread.sleep(1000);
+
+	// 5. when the counting is done, speak "blastoff!"
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
--- src/extra/ThatWasEasy.java
+++ src/extra/ThatWasEasy.java
@@ -18,8 +18,9 @@ public class ThatWasEasy extends MouseAdapter {
 	public void mouseClicked(MouseEvent arg0) {
 		/* Use the speak method to make the button work. */
 		
+		speak("words");
 	}
-
+	
 	private void speak(String words) {
 		try {
 			Runtime.getRuntime().exec("say " + words).waitFor();
